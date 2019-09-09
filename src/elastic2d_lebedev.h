7d97e318b3343bf9c848f2feae8b04a7c1721903
jianglq6 <jlq0608@gmail.com>
2019-09-09 14:56:32 +0800

    change file name

--- src/elastic2d_lebedev.h
+++ src/elastic2d_lebedev.h
@@ -28,3 +28,23 @@
 
 #define Dz10(a,fdx,ith,ix,iz)   \
     ( fdz[ith-1] * ( a[(iz+ ith   )*nx + ix] - a[(iz-(ith-1))*nx + ix] ) )
+
+
+int cal_momentum(float *fdx, float *fdz, int half_fd_stencil, int ni1, int ni2, int nk1, int nk2, int nx,
+                 float *hVx_1, float *hVz_1, float *Txx_1, float *Txz_1, float *Tzz_1,
+                 float *hVx_2, float *hVz_2, float *Txx_2, float *Txz_2, float *Tzz_2);
+
+/* RHS of stress-strain equation (Hook law) */
+int cal_hook(float *fdx, float *fdz, int half_fd_stencil, int ni1, int ni2, int nk1, int nk2, int nx,
+             float *hTxx_1, float *hTzz_1, float *hTxz_1, float *Vx_1 , float *Vz_1,
+             float *hTxx_2, float *hTzz_2, float *hTxz_2, float *Vx_2 , float *Vz_2,
+             float *c11_1 , float *c13_1 , float *c15_1 , float *c33_1, float *c35_1, float *c55_1,
+             float *c11_2 , float *c13_2 , float *c15_2 , float *c33_2, float *c35_2, float *c55_2);
+
+int update_velocity(int nx, int nz, float dt, float *Vx_1, float *Vz_1, float *Vx_2, float *Vz_2,
+        float *hVx_1, float *hVz_1,  float *hVx_2, float *hVz_2, float *B10, float *B01);
+
+int update_stress(int nx, int nz, float dt, float *Txx_1, float *Tzz_1, float *Txz_1,
+                  float *Txx_2 , float *Tzz_2 , float *Txz_2 ,
+                  float *hTxx_1, float *hTzz_1, float *hTxz_1,
+                  float *hTxx_2, float *hTzz_2, float *hTxz_2 );
